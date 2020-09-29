8c5134c1a37974be9db4a676ecdaf62945c3e8ab
DannyLines <dannylines@gmail.com>
2020-09-29 19:43:31 +0100

    Hello world

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {TodoList} from './TodoList.js';
 function App() {
   return(
     <TodoList />
     
+
   );
 }
 
